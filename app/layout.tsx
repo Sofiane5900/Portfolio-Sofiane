dd246a3b1c95876bcfd3a21c5e7ebaac8f3ec871
Sofiane5900 <132440690+Sofiane5900@users.noreply.github.com>
2024-06-14 19:50:13 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { ThemeProvider } from "@/components/theme-provider"
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -13,10 +16,19 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) { 
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
